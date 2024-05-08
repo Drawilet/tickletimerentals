6f4f73344e7cb7ebf557af66b84b2811dfbd852f
Drawilet <drawiletdev@gmail.com>
2024-05-08 09:30:00 -0500

    Update theme and animation configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,12 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            }, animation: {
-                'bounce-1': 'bounce 1s infinite',
-                'bounce-2': 'bounce 1s infinite 0.2s',
-                'bounce-3': 'bounce 1s infinite 0.4s',
-            }
+            },
+            animation: {
+                "bounce-1": "bounce 1s infinite",
+                "bounce-2": "bounce 1s infinite 0.2s",
+                "bounce-3": "bounce 1s infinite 0.4s",
+            },
         },
     },
 
@@ -30,8 +31,8 @@ module.exports = {
     ],
 
     daisyui: {
-        themes: ["winter", "dark"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "dark", // name of one of the included themes for dark mode
+        themes: ["corporate", "business"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: "business", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
         utils: true, // adds responsive and modifier utility classes
