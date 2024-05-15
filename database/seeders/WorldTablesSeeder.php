5c2c6910295ae6450917cb35f9a2e506e710984b
Drawilet <drawiletdev@gmail.com>
2024-05-15 11:01:43 -0500

    Add AcceptedLocationsSeeder and comment out unnecessary seeders

--- database/seeders/WorldTablesSeeder.php
+++ database/seeders/WorldTablesSeeder.php
@@ -22,8 +22,8 @@ class WorldTablesSeeder extends Seeder
         } else {
             \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         }
-		
-		ini_set('memory_limit', '512M');//allocate memory
+
+        ini_set('memory_limit', '512M');//allocate memory
         \DB::disableQueryLog();//disable log
 
         $this->call(WorldContinentsTableSeeder::class);
@@ -32,15 +32,15 @@ class WorldTablesSeeder extends Seeder
         $this->call(WorldCountriesLocaleTableSeeder::class);
         $this->call(WorldDivisionsTableSeeder::class);
         $this->call(WorldDivisionsLocaleTableSeeder::class);
-        $this->call(WorldCitiesTableSeeder::class);
-		$this->call(WorldCitiesTableSeederOne::class);
-		$this->call(WorldCitiesTableSeederTwo::class);
-		$this->call(WorldCitiesTableSeederThree::class);
-		$this->call(WorldCitiesTableSeederFour::class);
-		$this->call(WorldCitiesTableSeederFive::class);
-		$this->call(WorldCitiesTableSeederSix::class);
-        $this->call(WorldCitiesLocaleTableSeeder::class);
-        $this->call(WorldLanguagesTableSeeder::class);
+        /*    $this->call(WorldCitiesTableSeeder::class);
+           $this->call(WorldCitiesTableSeederOne::class);
+           $this->call(WorldCitiesTableSeederTwo::class);
+           $this->call(WorldCitiesTableSeederThree::class);
+           $this->call(WorldCitiesTableSeederFour::class);
+           $this->call(WorldCitiesTableSeederFive::class);
+           $this->call(WorldCitiesTableSeederSix::class);
+           $this->call(WorldCitiesLocaleTableSeeder::class);
+           $this->call(WorldLanguagesTableSeeder::class); */
 
         if (\DB::connection() instanceof SQLiteConnection) {
             \DB::statement('PRAGMA FOREIGN_KEYS=1');
