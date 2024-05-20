7aeef63ba39e6563d337439100d65af20968de5d
Alonzo997 <alonzojesuseduardo@gmail.com>
2024-05-20 13:04:34 -0500

    chore: Update tenants-component.blade.php to use table layout for better readability

--- resources/views/livewire/app/tenants-component.blade.php
+++ resources/views/livewire/app/tenants-component.blade.php
@@ -1,24 +1,30 @@
-<div class="flex flex-wrap gap-6">
-    @foreach ($tenants as $tenant)
-        <div class="card md:card-side shadow-xl p-4 flex-1 relative {{ $tenant->suspended ? 'scale-95 opacity-50 border border-primary' : '' }} w-full max-w-md max-h-56"
-            @if ($tenant->background_image) style="background: linear-gradient(rgba(0, 0, 0, 0.562), rgba(255, 255, 255, 0.1)), url('{{ $tenant->background_image }}'); background-size: cover;" @endif>
-            <figure class="mb-4">
-                <img src="{{ $tenant->profile_image }}" alt="Foto"
-                    class="w-32 h-32 object-cover rounded-full mx-auto" />
-            </figure>
-            <div class="card-body relative">
-                <h2 class="card-title text-2xl -mb-2">{{ $tenant->name }}</h2>
-                <p class="mb-4">{{ $tenant->email }}</p>
-                <div class="card-actions justify-end">
-                    <a class="btn btn-primary"
-                        href="{{ route('app.tenants.show', [
-                            'id' => $tenant->id,
-                        ]) }}">
-                        <x-icons.arrow-top-right-on-square />
-                    </a>
-
-                </div>
-            </div>
-        </div>
-    @endforeach
+<div class="overflow-x-auto mx-auto w-full">
+    <table class="table w-full">
+        <thead>
+            <tr>
+                <th>Profile Image</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tenants as $key => $tenant)
+                <tr class="{{ $tenant->suspended ? 'scale-95 opacity-50 border border-primary' : '' }}">
+                    <td>
+                        <div class="mask mask-squircle w-12 h-12">
+                            <img src="{{ $tenant->profile_image }}" alt="Foto" />
+                        </div>
+                    </td>
+                    <td>{{ $tenant->name }}</td>
+                    <td>{{ $tenant->email }}</td>
+                    <td>
+                        <a href="{{ route('app.tenants.show', ['id' => $tenant->id]) }}" class="btn btn-primary">
+                            <x-icons.arrow-top-right-on-square />
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
